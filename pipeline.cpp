33a5f1a30371380cffc05db9636ca959c60fb631
JoeK <ohhjassassin@gmail.com>
2023-03-02 18:32:22 +0000

    Fixed vert and frag file not being read

--- pipeline.cpp
+++ pipeline.cpp
@@ -22,22 +22,21 @@ namespace lve
 	{
 		std::ifstream file(filePath, std::ios::ate | std::ios::binary);
 
-		//this part is not working - check to see what is wrong
 		if (!file.is_open())
 		{
 			//filepath is incorrect or do not have permissions
 			throw runtime_error("failed to open file: " + filePath);
-
-			size_t fileSize = static_cast<size_t>(file.tellg());
-
-			vector<char> buffer(fileSize);
-
-			file.seekg(0);
-			file.read(buffer.data(), fileSize);
-
-			file.close();
-			return buffer;
 		}
+
+		size_t fileSize = static_cast<size_t>(file.tellg());
+
+		vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+		return buffer;
 	}
 
 	/*void PipeLine::createGraphicsPipeline(
