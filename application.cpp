7449d4568e9eebc3418d5b1b72fd66de8fdba5c6
JoeKep-git <joeykeputabusiness@gmail.com>
2023-03-31 17:37:23 +0100

    multiple blue triangles displayed

--- application.cpp
+++ application.cpp
@@ -2,8 +2,18 @@
 #include <stdexcept>
 #include <array>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 namespace lve
 {
+	struct SimplePushConstantData
+	{
+		glm::vec2 offset;
+		alignas(16) glm::vec3 color;
+	};
+
 	FirstApplication::FirstApplication()
 	{
 		loadModels();
@@ -41,12 +51,17 @@ namespace lve
 
 	void FirstApplication::createPipelinelayout()
 	{
+		VkPushConstantRange pushConstantRange{};
+		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+		pushConstantRange.offset = 0;
+		pushConstantRange.size = sizeof(SimplePushConstantData);
+
 		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
 		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
 		pipelineLayoutInfo.setLayoutCount = 0;
 		pipelineLayoutInfo.pSetLayouts = nullptr;
-		pipelineLayoutInfo.pushConstantRangeCount = 0;
-		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount = 1;
+		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
 		if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
 			VK_SUCCESS)
@@ -127,6 +142,9 @@ namespace lve
 
 	void FirstApplication::recordCommandBuffer(int imageIndex)
 	{
+		static int frame = 0;
+		frame = (frame + 1) % 1000;
+
 		VkCommandBufferBeginInfo beginInfo{};
 
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
@@ -145,8 +163,8 @@ namespace lve
 		renderPassInfo.renderArea.extent = lveSwapChain->getSwapChainExtent();
 
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
-		//clearValues[0].depthStencil =
+		clearValues[0].color = { 0.01f, 0.01f, 0.01f, 1.0f };
+		clearValues[1].depthStencil = { 1.0f, 0 };
 		renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
 		renderPassInfo.pClearValues = clearValues.data();
 
@@ -167,7 +185,25 @@ namespace lve
 
 		lvePipeline->bind(commandBuffers[imageIndex]);
 		model->bind(commandBuffers[imageIndex]);
-		model->draw(commandBuffers[imageIndex]);
+
+		for (int i = 0; i < 4; i++)
+		{
+			SimplePushConstantData push{};
+
+			push.offset = { -0.5f + frame * 0.002f, -0.4f + i * 0.25f };
+			push.color = { 0.0f, 0.0f, 0.2f + 0.2f * i };
+
+			vkCmdPushConstants(commandBuffers[imageIndex], 
+				pipelineLayout, 
+				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 
+				0, 
+				sizeof(SimplePushConstantData), 
+				&push);
+
+			model->draw(commandBuffers[imageIndex]);
+
+		}
+
 
 		vkCmdEndRenderPass(commandBuffers[imageIndex]);
 		if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS)
